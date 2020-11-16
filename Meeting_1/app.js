b0d6a23eb9745ce227ab1f651900da841d960cf0
serhii <sna@lindenvalley.de>
2020-11-17 01:26:07 +0200

    add second commit

--- Meeting_1/app.js
+++ Meeting_1/app.js
@@ -1,6 +1,8 @@
 const http = require("http");
-http.createServer((req, resp)=> {
-resp.end("Hellp");
-}).listen(3000, "localhost", ()=> {
+http
+  .createServer((req, resp) => {
+    resp.end("Hello world from js");
+  })
+  .listen(3000, "localhost", () => {
     console.log("Server starts in 3000 port");
-});
+  });
