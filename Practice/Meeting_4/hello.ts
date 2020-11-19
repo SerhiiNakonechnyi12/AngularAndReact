17cf8838795087cacbc55ed194cb8f4e84bb3eb0
serhii <sna@lindenvalley.de>
2020-11-19 18:41:09 +0200

    add code

--- Practice/Meeting_4/hello.ts
+++ Practice/Meeting_4/hello.ts
@@ -1,2 +1,8 @@
-let hello: string = "Hello world of TS!";
-let x : number 
+var hello: string = "Hello world of TS!";
+var x = 12 ;
+var a;
+a = 17;
+console.log(hello);
+console.log(a);
+a = "Typescript is aaawqqqe";
+console.log(a);
