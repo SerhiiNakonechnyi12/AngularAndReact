76e020e50df09fcd71f20043617e31ce01e520b0
serhii <sna@lindenvalley.de>
2020-11-25 18:59:08 +0200

    test

---
+++ Practice/ToDoListApp/src/app/to-do-list.ts
@@ -0,0 +1,9 @@
+import { ToDoListItem } from './to-do-list-item';
+
+export const ITEMS: ToDoListItem[] =
+[
+    {id: 1, name: "Позвонить Вадиму", isCompleted: false},
+    {id: 2, name: "Сделать дз", isCompleted: false},
+    {id: 3, name: "Сделать практику", isCompleted: false},
+
+]
