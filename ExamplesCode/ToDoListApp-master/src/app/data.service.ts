c75af733aca1a749d7afc68fe5e423c4b1d9553b
serhii <sna@lindenvalley.de>
2020-12-01 13:04:55 +0200

    Add part of lessons 9

--- ExamplesCode/ToDoListApp-master/src/app/data.service.ts
+++ ExamplesCode/ToDoListApp-master/src/app/data.service.ts
@@ -1,11 +1,15 @@
+import {MyLogService} from "./my-log.service"
 export class DataService{
     private data: string[] = ["Apple iPhone 12", "POCO 3", "Samsung Note 20"];
 
+    constructor(private logger: MyLogService){}
     getData(){
+        this.logger.logMessage("Операция получения данных");
         return this.data;
     }
 
     addItem(item: string){
         this.data.push(item);
+        this.logger.logMessage("Операция добавления данных");
     }
 }
---
+++ ExamplesCode/ToDoListApp-master/src/app/my-log.service.ts
@@ -0,0 +1,5 @@
+export class MyLogService{
+    logMessage(message: string){
+        console.log(message);
+    }
+}
