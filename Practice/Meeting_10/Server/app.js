c0533ad972d8118903efc71905fff79e11ac634c
serhii <sna@lindenvalley.de>
2020-12-02 01:34:27 +0200

    add server lisner

--- Practice/Meeting_10/Server/app.js
+++ Practice/Meeting_10/Server/app.js
@@ -2,5 +2,14 @@ const { request, response } = require("express");
 const express = require("express");
 const app = express();
 app.get("/sum", (request, response)=>{
-
+    let num1 = parseInt(request.query.num1);
+    let num2 = parseInt(request.query.num2);
+    let sum = num1 + num2;
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    response.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+    response.send({result: sum});
 });
+app.listen(3000, ()=>{
+    console.log("Server is up! Listen port 3000")
+})
