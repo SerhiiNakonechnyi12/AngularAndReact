c796241a1e97dcac025ffc9cba4812b6aba66bc9
serhii <sna@lindenvalley.de>
2020-12-07 18:55:24 +0200

    test

--- ReactApps/propsusageexample/src/RandomNumber.js
+++ ReactApps/propsusageexample/src/RandomNumber.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function RandomNumber(props) {
   let min = props.min;
   let max = props.max;
+  let qwe;
   let rnd = Math.floor(Math.random() * (max - min + 1) + min);
   return (<h3>Случайное число: {rnd}</h3>);
 }
